9810b977df54cecc9b0229e5f7257dfe1f22d004
mcquenji <benjamin.mceachnie@necode.it>
2024-10-14 10:12:51 +0000

    feat: port flutter specific utils from mcquenji_core

---
+++ README.md
@@ -0,0 +1,12 @@
+# Flutter Utils
+
+Flutter utilties for common tasks.
+
+## Usage
+
+```yaml
+dependencies:
+  flutter_utils:
+    git:
+      url: git://github.com/mcquenji/flutter_utils.git
+```
---
+++ lib/flutter_utils.dart
@@ -0,0 +1,12 @@
+library flutter_utils;
+
+export 'src/animation_utils.dart';
+export 'src/declarative_padding.dart';
+export 'src/hover_builder.dart';
+export 'src/hoverable_widget.dart';
+export 'src/offset_on_hover.dart';
+export 'src/routing_utils.dart';
+export 'src/scale_on_hover.dart';
+export 'src/spacing.dart';
+export 'src/transform_on_hover.dart';
+export 'src/widget_utils.dart';
